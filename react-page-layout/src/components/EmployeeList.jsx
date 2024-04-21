67a7b7ed8f8802a4410198775efcb64a5797a42c
Fatu2024 <fatumab3@gmail.com>
2024-04-20 23:22:39 -0400

    employee list is rendering

--- react-page-layout/src/components/EmployeeList.jsx
+++ react-page-layout/src/components/EmployeeList.jsx
@@ -1,7 +1,23 @@
 import EmployeeListItem from './EmployeeListItem'
 
 export default function EmployeeList () {
+    const employees =[
+        {name:'Fatu Musa', position: 'President and CEO'},
+        {name:'Julie Taylor', position: 'VP of Marketing'},
+        {name:'Eugene Lee', position: 'CFO'},
+        {name:'John Williams', position: 'VP of Engineering'},
+        {name:'Ray Moore', position: 'VP of Sales'},
+        {name:'Paul Jones', position: 'QA Manager'}
+
+    ];
     return (
-        <EmployeeListItem />
+        <>
+        <div className='employee-list'>
+            {employees.map((employee, index) => (
+                <EmployeeListItem key={index} employee={employee} />
+            ))}
+        </div>
+        </>
     )
 }
+
--- react-page-layout/src/components/EmployeeListItem.jsx
+++ react-page-layout/src/components/EmployeeListItem.jsx
@@ -1,10 +1,15 @@
-export default function EmployeeListItem () {
-    const employee = () => {
-        
-    }
+import React from "react"
+
+export default function EmployeeListItem( { employee }) {
     return (
         <>
-        
+            <div className="employee-list">
+                {/* <img src="" /> */}
+                <div className="employee-details">
+                    <h3>Name: {employee.name}</h3>
+                    <h4> <i>Position: {employee.position} </i></h4>
+                </div>
+            </div>
         </>
     )
 }
